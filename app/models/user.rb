b4606bef3847ac9d6d1f79a20bf9f866717b41fc
statelydigger <statelydigger-reg@yahoo.com>
2010-07-19 21:50:08 -0400

    In part II of models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -7,6 +8,14 @@ class User < ActiveRecord::Base
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  # Automatically create the virtual attribute 'password_confirmation'
+  validates_confirmation_of :password
+
+  # Password validations
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
 
 end
 
